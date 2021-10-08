96fb61d13cd8a84c5841e3921913937190acee97
Abhishek Naik <abhisheknaik22296@gmail.com>
2021-10-08 17:30:40 +0530

    experiments have multiple seeds, are reproducible

--- dqn.py
+++ dqn.py
@@ -12,11 +12,11 @@ import numpy as np
 
 
 def get_stats(results):
-    mean = np.mean(results)
-    std = np.std(results)
-    min = np.min(results)
-    max = np.max(results)
-    median = np.median(results)
+    mean = np.round(np.mean(results), 2)
+    std = np.round(np.std(results), 2)
+    min = np.round(np.min(results), 2)
+    max = np.round(np.max(results), 2)
+    median = np.round(np.median(results), 2)
     return mean, std, min, max, median
 
 
@@ -58,13 +58,16 @@ class DQNAgent:
         self.last_obs = None
         self.last_action = None
         self.counter = None
-        self.model_save_loc_name = None
+        self.save_model = None
+        self.save_model_loc = None
 
     def agent_init(self, agent_info):
 
         self.seed = agent_info.get('rng_seed', 42)
-        self.rng = torch.Generator()
-        self.rng.manual_seed(self.seed)
+        # self.rng = torch.Generator()
+        # self.rng.manual_seed(self.seed)
+        torch.manual_seed(self.seed)
+        random.seed(self.seed)  # ToDo: don't like this. Remove need for this package
 
         self.gamma = torch.tensor(agent_info.get('gamma', 0.95)).float().to(device)
         self.epsilon = torch.tensor(agent_info.get('epsilon', 0.2)).float().to(device)
@@ -79,6 +82,7 @@ class DQNAgent:
         self.load_model_from = agent_info.get('load_model_from', None)
         if self.load_model_from:
             self.q_net.load_state_dict(torch.load(self.load_model_from))
+            print(f'Successfully loaded model from {self.load_model_from}')
         self.target_net = copy.deepcopy(self.q_net).to(self.device)
         self.net_sync_freq = agent_info.get('net_sync_freq', 256)
         self.net_sync_counter = 0
@@ -93,14 +97,16 @@ class DQNAgent:
         self.experience_buffer = deque(maxlen=self.buffer_size)
 
         self.counter = 0
-        self.model_save_loc_name = agent_info.get('model_save_loc_name', 'models/cartpole-dqn.pth')
+        self.save_model = agent_info.get('save_model', False)
+        self.save_model_loc = agent_info.get('save_model_loc', 'models/cartpole-dqn')
 
-    def save_trained_model(self):
-        torch.save(self.q_net.state_dict(), self.model_save_loc_name)
+    def save_trained_model(self, suffix):
+        filename = self.save_model_loc + suffix + '.pth'
+        torch.save(self.q_net.state_dict(), filename)
 
     def choose_action(self, state):
         assert state.ndim == 1, "get_action expects to return the action for a single state, not a vector of states"
-        if torch.rand(1).item() < self.epsilon:
+        if torch.rand(1) < self.epsilon:
             action = torch.randint(0, self.num_actions, (1,))
         else:
             with torch.no_grad():   # because gradients not required here
@@ -187,76 +193,107 @@ class DQNAgent:
 
         return loss.item()
 
+    # def close(self):
+
+
 
 def experiment_loop(exp_info, env_info, agent_info):
-    env = gym.make('CartPole-v0')
-    input_dim = env.observation_space.shape[0]
-    output_dim = env.action_space.n
-    agent = DQNAgent(config={'num_features': input_dim, 'num_actions': output_dim})
-    agent.agent_init(agent_info)
-
     # Main training loop
-    losses_list, reward_list, episode_len_list, epsilon_list = [], [], [], []
+    # losses_list, reward_list, episode_len_list, epsilon_list = [], [], [], []
+    num_runs = exp_info.get('num_runs', 5)
     num_episodes = exp_info.get('num_episodes', 10000)
+    returns_all = np.zeros((num_runs, num_episodes))
     render = env_info.get('render', False)
 
-    for i in tqdm(range(num_episodes)):
-        obs, done, losses, eps_len, eps_ret = env.reset(), False, 0, 0, 0
-        action = agent.agent_start(obs)
-        if render:
-            env.render()
-            time.sleep(0.01)
+    for run in range(num_runs):
 
-        while not done:
-            eps_len += 1
-            obs_next, reward, done, _ = env.step(action)
-            action, loss = agent.agent_step(reward, obs_next, False)
-            eps_ret += reward
-            if loss is not None:
-                losses += loss
+        env = gym.make('CartPole-v0')
+        env.seed(run)
+        input_dim = env.observation_space.shape[0]
+        output_dim = env.action_space.n
+        agent = DQNAgent(config={'num_features': input_dim, 'num_actions': output_dim})
+        agent_info['rng_seed'] = run
+        agent.agent_init(agent_info)
+
+        for eps in tqdm(range(num_episodes)):
+            obs, done, losses, eps_len, eps_ret = env.reset(), False, 0, 0, 0
+            action = agent.agent_start(obs)
             if render:
                 env.render()
                 time.sleep(0.01)
 
-        losses_list.append(losses / eps_len), reward_list.append(eps_ret)
-        episode_len_list.append(eps_len), epsilon_list.append(agent.epsilon)
+            while not done:
+                eps_len += 1
+                obs_next, reward, done, _ = env.step(action)
+                action, loss = agent.agent_step(reward, obs_next, False)
+                eps_ret += reward
+                if loss is not None:
+                    losses += loss
+                if render:
+                    env.render()
+                    time.sleep(0.01)
 
-    # print(f'Average episode length: {sum(episode_len_list)/num_episodes}')
-    # print(f'Average episode length (last half of training): {sum(episode_len_list[num_episodes//2:])/(num_episodes/2)}')
-    print(f'Stats (mean, std, min, max, median): {get_stats(episode_len_list)}')
-    print(f'Stats (last half): {get_stats(episode_len_list[num_episodes//2:])}')
-    print("Saving trained model")
-    agent.save_trained_model()
+            # losses_list.append(losses / eps_len), reward_list.append(eps_ret)
+            # episode_len_list.append(eps_len), epsilon_list.append(agent.epsilon)
+            returns_all[run][eps] = eps_ret
 
+        # print(f'Average episode length: {sum(episode_len_list)/num_episodes}')
+        # print(f'Average episode length (last half of training): {sum(episode_len_list[num_episodes//2:])/(num_episodes/2)}')
+        print(f'Stats (mean, std, min, max, median): {get_stats(returns_all[run])}')
+        print(f'Stats (last half): {get_stats(returns_all[run, num_episodes//2:])}')
+        if agent.save_model:
+            print("Saving trained model")
+            suffix = f'_{run}'
+            agent.save_trained_model(suffix)
+        env.close()
+
+    print(f'\n\nFinal stats (mean, std, min, max, median): {get_stats(returns_all)}')
+    print(f'Final stats (last 50%): {get_stats(returns_all[:, num_episodes//2:])}')
+    print(f'Final stats (last 10%): {get_stats(returns_all[:, int(0.9*num_episodes):])}')
+
+
+def run(save_model_loc=None, load_model_from=None, device='cpu', mode='eval'):
+
+    if mode=='train':
+        epsilon = 1.0
+        step_size = 1e-3
+        save_model = True
+        num_runs = 5
+        num_episodes = 10000
+        param_update_freq = 32
+        render = False
+    else:
+        epsilon = 0.0
+        step_size = 0.0
+        save_model = False
+        num_runs = 1
+        num_episodes = 10
+        param_update_freq = 10000
+        render = True
+
+    experiment_loop(
+        exp_info={'num_episodes': num_episodes,
+                  'num_runs': num_runs},
+        env_info={'render': render},
+        agent_info={'layer_sizes': [4, 64, 2],
+                    'step_size': step_size,
+                    'epsilon': epsilon,
+                    'rng_seed': 1423,
+                    'net_sync_freq': 128,
+                    'buffer_size': 256,
+                    'param_update_freq': param_update_freq,
+                    'batch_size': 16,
+                    'device': device,
+                    'save_model': save_model,
+                    'save_model_loc': save_model_loc,
+                    'load_model_from': load_model_from}
+    )
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # experiment_loop(
-    #     exp_info={'num_episodes':10000},
-    #     env_info={'render': False},
-    #     agent_info={'layer_sizes': [4, 64, 2],
-    #                 'step_size': 1e-3,
-    #                 'epsilon': 1.0,
-    #                 'rng_seed': 1423,
-    #                 'net_sync_freq': 128,
-    #                 'buffer_size': 256,
-    #                 'param_update_freq': 32,
-    #                 'batch_size': 16,
-    #                 'device': device,
-    #                 'model_save_loc_name': 'models/cartpole-dqn.pth'}
-    # )
 
-    experiment_loop(
-        exp_info={'num_episodes': 10},
-        env_info={'render': True},
-        agent_info={'layer_sizes': [4, 64, 2],
-                    'step_size': 0.0,
-                    'epsilon': 0.0,
-                    'load_model_from': 'cartpole-dqn.pth',
-                    'rng_seed': 1423,
-                    'net_sync_freq': 10000,
-                    'buffer_size': 256,
-                    'param_update_freq': 10000,
-                    'batch_size': 16,
-                    'device': device}
-    )
+    # # run('models/cartpole-dqn', device=device, render=False, num_episodes=10000, num_runs=5, save_model=True)
+    # run('models/cartpole-dqn.pth_4.pth', device=device, render=True, num_episodes=50, num_runs=1, save_model=False)
+
+    run(save_model_loc='models/cartpole-dqn', device=device, mode='train')
+    # run(load_model_from='models/cartpole-dqn_4.pth', device=device, mode='eval')
